bf8f12265446a2163c2b7be321fad21b44418eb1
programming 2 <programming@irontigers.com>
2019-02-10 13:26:10 -0500

    Encoder 3 values

--- src/main/java/com/irontigers/commands/ElevatorLevel1Dropoff.java
+++ src/main/java/com/irontigers/commands/ElevatorLevel1Dropoff.java
@@ -6,24 +6,37 @@ public class ElevatorLevel1Dropoff extends Command {
 
   public ElevatorLevel1Dropoff(){
     // TODO: add all systems this command will use
+ double dropOff1 = 123;
+    // TODO: add all systems this command will use
     // requires(ElevatorSystem.instance());
   }
 
   @Override
   protected void execute() {
-    // TODO: implement
+    if(ElevatorSystem.instance().getRawPosition() > 1.05 * dropOff1){
+        ElevatorSystem.instance().move(-.5);
+    }
+    else if(ElevatorSystem.instance().getRawPosition() < .95 * dropOff1){
+       ElevatorSystem.instance().move(.5);
+    }
+    else{
+      ElevatorSystem.instance().stop();
+    }
   }
 
   @Override
   protected boolean isFinished() {
-    // This should execute exactly once
-    return true;
+    if(.95 * dropOff1 < ElevatorSystem.instance().getRaw && 1.05 * dropOff1 > ElevatorSystem.instance().getRaw){
+        return true;
+    }
+    else{return false};
   }
 
   // Called once after isFinished returns true
   @Override
   protected void end() {
     // TODO: if needs an end command, code it here
+    ElevatorSystem.instance().stop();
   }
 
   // Called when another command which requires one or more of the same
@@ -32,4 +45,3 @@ public class ElevatorLevel1Dropoff extends Command {
   protected void interrupted() {
     end();
   }
-}
--- src/main/java/com/irontigers/commands/ElevatorLevel3.java
+++ src/main/java/com/irontigers/commands/ElevatorLevel3.java
@@ -5,25 +5,37 @@ import edu.wpi.first.wpilibj.command.Command;
 public class ElevatorLevel3 extends Command {
 
   public ElevatorLevel3(){
+double positionDropOff3 = 123;
     // TODO: add all systems this command will use
     // requires(ElevatorSystem.instance());
   }
 
   @Override
   protected void execute() {
-    // TODO: implement
+    if(ElevatorSystem.instance().getRawPosition() > 1.05 * positionDropOff3){
+        ElevatorSystem.instance().move(-.5);
+    }
+    else if(ElevatorSystem.instance().getRawPosition() < .95 * positionDropOff3){
+       ElevatorSystem.instance().move(.5);
+    }
+    else{
+      ElevatorSystem.instance().stop();
+    }
   }
 
   @Override
   protected boolean isFinished() {
-    // This should execute exactly once
-    return true;
+    if(.95 * positionDropOff3 < ElevatorSystem.instance().getRaw && 1.05 * positionDropOff3 > ElevatorSystem.instance().getRaw){
+        return true;
+    }
+    else{return false};
   }
 
   // Called once after isFinished returns true
   @Override
   protected void end() {
     // TODO: if needs an end command, code it here
+    ElevatorSystem.instance().stop();
   }
 
   // Called when another command which requires one or more of the same
@@ -32,4 +44,3 @@ public class ElevatorLevel3 extends Command {
   protected void interrupted() {
     end();
   }
-}
