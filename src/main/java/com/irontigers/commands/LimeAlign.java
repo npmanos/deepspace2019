fe5701b29f90572b709a23869ebfdd9135be92d0
Nick Manos <nmanos@eastonrobotics.org>
2019-02-25 02:47:19 +0000

    Change LimeAlign slow down logic and add additional debug output
    Check if y is less than 1.5 first, rather than after to ensure speed is
    not set back to .35 before changing to slowdown. Also simplify math so
    that the speed immediately slows down to .25 and then continues to
    decrease as it gets closer
    
    Add whether the limelight currently has targets to debug output

--- src/main/java/com/irontigers/commands/LimeAlign.java
+++ src/main/java/com/irontigers/commands/LimeAlign.java
@@ -17,13 +17,8 @@ import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
 public class LimeAlign extends Command {
   private double x;
   private double y;
-  private double yaw;
   private boolean hasTargets;
-  private double[] threeDeeOut;
-  private double areaLeft;
-  private double areaRight;
-  private RollingAverage averageLeftArea = new RollingAverage(5);
-  private RollingAverage averageRightArea = new RollingAverage(5);
+  private boolean stillHasTargets;
   NetworkTable limelight;
 
   public LimeAlign() {
@@ -35,21 +30,20 @@ public class LimeAlign extends Command {
     limelight = NetworkTableInstance.getDefault().getTable("limelight");
     limelight.getEntry("pipeline").setNumber(0);
     hasTargets = false;
+    stillHasTargets = false;
     Robot.instance().enableStandardControl();
     DashboardPublisher.instance().putDriver("Driving with Vision", true);
     Shuffleboard.addEventMarker("Vision Alignment Enabled", EventImportance.kLow);
   }
 
-  private List<Double> leftArea = new ArrayList<Double>();
-  private List<Double> rightArea = new ArrayList<Double>();
-
   // Called repeatedly when this Command is scheduled to run
   @Override
   protected void execute() {
-    // threeDeeOut = table.getEntry("camtran").getDoubleArray(new double[6]);
-
     if(limelight.getEntry("tv").getDouble(0.0) == 1){
       hasTargets = true;
+      stillHasTargets = true;
+    }else{
+      stillHasTargets = false;
     }
 
     double forwardSpeed = 0;
@@ -59,25 +53,18 @@ public class LimeAlign extends Command {
     x = limelight.getEntry("tx").getDouble(0.0);
     y = limelight.getEntry("ty").getDouble(0.0);
 
-    // areaLeft = averageLeftArea.getAverage();
-    // areaRight = averageRightArea.getAverage();
-    // yaw = threeDeeOut[4];
-    // double a = table.getEntry("ta").getDouble(0.0);
-
     if(x < -.37){
       strafeSpeed = -.35;
     }
     else if(x > .37){
       strafeSpeed = .35;
     }
-    if(y < -.37){
+    if(y < 1.5 && y > .37){
+      forwardSpeed = .167 * y;
+    }else if(y < -.37){
       forwardSpeed = -.35;
     }else if(y > .37){
       forwardSpeed = .35;
-    }
-
-    if(y < 1.5){
-      forwardSpeed = forwardSpeed * .35 * y;
     }
 
     rotateSpeed = .005 * x;
@@ -86,7 +73,7 @@ public class LimeAlign extends Command {
 
     DashboardPublisher.instance().putDebug("Limelight X", x);
     DashboardPublisher.instance().putDebug("Limelight Y", y);
-    // DashboardPublisher.instance().put("Limelight Yaw", yaw);
+    DashboardPublisher.instance().putDebug("Limelight has target", stillHasTargets);
 
     DriveSystem.instance().drive(forwardSpeed, strafeSpeed, rotateSpeed);
 
@@ -101,10 +88,7 @@ public class LimeAlign extends Command {
   // Make this return true when this Command no longer needs to run execute()
   @Override
   protected boolean isFinished() {
-    // This is our standard default command so we're never going to be done
-    System.out.println(limelight.getEntry("camMode").getDouble(1.0));
     return (x > -.4 && x < .4 && y > -.4 && y < .4 && hasTargets);
-    // return false;
   }
 
   // Called once after isFinished returns true
